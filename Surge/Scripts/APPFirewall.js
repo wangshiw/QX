b5ed13483caeb845e6c5c6c290cb2135a8fca8cb
wangshiw <51616825+wangshiw@users.noreply.github.com>
2022-05-05 15:32:46 +0800

    Create APPFirewall.js

---
+++ Surge/Scripts/APPFirewall.js
@@ -0,0 +1,8 @@
+/*
+
+APP防火墙 = type=cron,cronexp="0 0 1 1 *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/wangshiw/QX/master/Surge/Scripts/APPFirewall.js
+
+*/
+const data = $intent.parameter;
+const write = $persistentStore.write(data, "APP_BJ");
+$done();
